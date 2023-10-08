e99a3d8509aceb3e2bff69ad86cd21e3bf2c8eef
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-10-08 18:40:58 +0200

    Jakiś postęp

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,6 +4,7 @@ const config = require("../config");
 const connection = mysql.createConnection(config.db);
 
 function queryDatabase(query, params, callback) {
+  console.log("DB module loaded!");
   connection.query(query, params, callback);
 }
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
-const queryDatabase = require("../db/index");
+const { queryDatabase } = require("../db");
+console.log("Query DB Function: ", queryDatabase);
 
 const getUsers = async () => {
   try {
@@ -11,9 +12,11 @@ const getUsers = async () => {
 
 const findUserByEmail = (email, callback) => {
   queryDatabase("SELECT * FROM Pracownicy WHERE Mail = ?", [email], callback);
+  console.log("Funkcja 1");
 };
 const findUserPasswordByID = (id, callback) => {
   queryDatabase("SELECT * FROM Login WHERE Pracownik_ID = ?", [id], callback);
+  console.log("Funkcja 2");
 };
 
 // Inne funkcje dotyczące operacji na użytkownikach...
