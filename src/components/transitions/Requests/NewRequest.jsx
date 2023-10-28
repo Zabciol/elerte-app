570e85f18f6f49b2bed25b9858b6a1cde3087ae4
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-10-28 22:08:19 +0200

    Update NewRequest.jsx

--- src/components/transitions/Requests/NewRequest.jsx
+++ src/components/transitions/Requests/NewRequest.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { mySupervisorsAPI } from "../../../api/employeesApi";
 const NewRequest = (props) => {
   const [mySupervisors, setMySupervisors] = useState([]);
+  const [mySupervisor, setMySupervisor] = useState();
   const getMySupervisors = async () => {
     const data = await mySupervisorsAPI(props.user.ID);
     console.log(data);
     setMySupervisors(data);
+    setMySupervisor(data[0]);
   };
 
   useEffect(() => {
