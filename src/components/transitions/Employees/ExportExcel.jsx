7b7139504478e261243839a06ae46aac4acc9704
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-06 14:44:08 +0100

    Update ExportExcel.jsx
    Dodałem dyrekcje żeby mogli wyciągać ecp całej firmy

--- src/components/transitions/Employees/ExportExcel.jsx
+++ src/components/transitions/Employees/ExportExcel.jsx
@@ -4,26 +4,29 @@ import { allEmployeesAPI } from "../../../api/employeesApi";
 import { exportECPAPI } from "../../../api/ecpApi";
 
 const ExportExcel = (props) => {
-  const [employeesID, setEmployeesID] = useState(
-    props.subordinates.map((employee) => employee.ID)
-  );
+  const [employeesID, setEmployeesID] = useState([]);
 
   const download = async () => {
-    const result = await exportECPAPI(props.date, employeesID);
-    console.log("Pobieranie");
+    await exportECPAPI(props.date, employeesID);
   };
 
   const getAllEmployees = async () => {
     const data = await allEmployeesAPI();
-    console.log(data.map((employee) => employee.ID));
     setEmployeesID(data.map((employee) => employee.ID));
   };
 
   useEffect(() => {
-    if (props.user.Dzial === "Księgowość" || props.user.Dzial === "Analityka") {
+    if (
+      props.user.Dzial === "Księgowość" ||
+      props.user.Dzial === "Analityka" ||
+      props.user.Dzial === "Dyrekcja"
+    ) {
       getAllEmployees();
+    } else {
+      const IDs = props.subordinates.map((employee) => employee.ID);
+      setEmployeesID(IDs);
     }
-  }, []);
+  }, [props]);
   return (
     <div>
       <Button variant='primary' onClick={download}>
