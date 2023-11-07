8d5b20b63dcfa5e6b906bf029d01ecc76ed82063
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-07 12:32:10 +0100

    Update EmployeesList.jsx
    Komunikat brak danych

--- src/components/transitions/Employees/EmployeesList.jsx
+++ src/components/transitions/Employees/EmployeesList.jsx
@@ -7,18 +7,20 @@ const EmployeesList = (props) => {
 
   return (
     <Accordion className='scroll'>
-      {subordinates.map((employee) => (
-        <EmployeeListItem employee={employee} date={date} key={employee.ID}>
-          {React.Children.map(children, (child) => {
-            if (React.isValidElement(child)) {
-              return React.cloneElement(child, {
-                employee: employee,
-              });
-            }
-            return child;
-          })}
-        </EmployeeListItem>
-      ))}
+      {subordinates.length > 0 ? (
+        subordinates.map((employee) => (
+          <EmployeeListItem employee={employee} date={date} key={employee.ID}>
+            {React.Children.map(children, (child) => {
+              if (React.isValidElement(child)) {
+                return React.cloneElement(child, { employee: employee });
+              }
+              return child;
+            })}
+          </EmployeeListItem>
+        ))
+      ) : (
+        <div className='no-data-message'>Brak danych</div>
+      )}
     </Accordion>
   );
 };
