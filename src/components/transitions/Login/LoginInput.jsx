a962597bd2cb5a2e43448e240f15078b35163825
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-10 11:08:02 +0100

    Update LoginInput.jsx

--- src/components/transitions/Login/LoginInput.jsx
+++ src/components/transitions/Login/LoginInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { useAccordionButton } from "react-bootstrap/AccordionButton";
 import Card from "react-bootstrap/Card";
@@ -7,34 +7,54 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 
 const LoginInput = (props) => {
+  const mailRef = useRef("");
+  const passwordRef = useRef("");
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [showToggle, setShowToggle] = useState(false);
   const [openAccordion, setOpenAccordion] = useState(false);
 
   const changeMail = (e) => {
-    setMail(e.target.value);
-    if (password === "") setShowToggle(true);
+    if (mailRef.current.value !== "") setShowToggle(true);
+    else setShowToggle(false);
   };
 
   const changePassword = (e) => {
-    setPassword(e.target.value);
-    setShowToggle(false);
+    if (mailRef.current.value !== "" && passwordRef.current.value !== "") {
+      setShowToggle(false);
+    } else {
+      setShowToggle(true);
+    }
   };
 
   const submit = () => {
-    props.submit(mail, password);
+    props.submit(mail, passwordRef.current.value);
   };
   const toggleAccordion = () => {
     setOpenAccordion(!openAccordion);
   };
 
   useEffect(() => {
-    if (password === "") {
-      setOpenAccordion(false);
-      setShowToggle(true);
-    }
-  }, [password]);
+    mailRef.current.addEventListener("keypress", (e) => {
+      if (e.key === "Enter" && mailRef.current.value !== "") {
+        setOpenAccordion(true);
+        e.preventDefault();
+        passwordRef.current.focus();
+      }
+    });
+    passwordRef.current.addEventListener("keypress", (e) => {
+      if (e.key === "Enter" && passwordRef.current.value !== "") {
+        e.preventDefault();
+        props.submit(mailRef.current.value, passwordRef.current.value);
+      }
+    });
+
+    return () => {
+      if (mailRef.current) {
+        mailRef.current.removeEventListener("keypress", this);
+      }
+    };
+  }, []);
 
   return (
     <Accordion activeKey={openAccordion ? "0" : null} data-bs-theme='light'>
@@ -44,10 +64,10 @@ const LoginInput = (props) => {
         data-bs-theme='light'
         className='mb-3'>
         <Form.Control
+          ref={mailRef}
           type='email'
           placeholder='name@example.com'
           onChange={changeMail}
-          value={mail}
         />
         {showToggle ? (
           <img
@@ -61,12 +81,12 @@ const LoginInput = (props) => {
       <Accordion.Collapse eventKey='0'>
         <FloatingLabel controlId='floatingPassword' label='Password'>
           <Form.Control
+            ref={passwordRef}
             type='password'
             placeholder='Password'
             onChange={changePassword}
-            value={password}
           />
-          {mail && password ? (
+          {mailRef.current.value && passwordRef.current.value ? (
             <img
               src={arrow}
               alt='arrow'
