56790c2863fc95bdec1fed1c79adc10b49e6884d
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-14 13:05:50 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,6 @@ const environment = process.argv[2] || process.env.NODE_ENV || "elerte";
 let config;
 try {
   config = require(`./config.${environment}.js`);
-  console.log(config);
 } catch (error) {
   console.error(
     `Could not load config for environment '${environment}', using local config as fallback.`
