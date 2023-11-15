987a575a6c8b3106e97ce23e47935c3b79742a97
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-15 10:36:31 +0100

    Progres bar

--- src/components/transitions/Home/Presence.jsx
+++ src/components/transitions/Home/Presence.jsx
@@ -10,7 +10,7 @@ const Presence = ({ user, date }) => {
     try {
       const data = await hoursWorkedApi(user.ID, date);
       console.log(data[0]);
-      setPercentWorkedHours(data[0].SumaGodzin);
+      setPercentWorkedHours(data[0].SumaGodzin / 160);
     } catch (error) {
       console.log(error);
       setMessage(error.message);
@@ -23,12 +23,15 @@ const Presence = ({ user, date }) => {
   }, []);
 
   return (
-    <div className='pt-3 text-white'>
+    <div className='p-5 text-white'>
       <h3>Obecność</h3>
       <div className='d-flex justify-content-between align-items-center '>
         <div className='w-100'>
           {" "}
-          <ProgressBar now={now} label={`${now}%`} />;
+          <ProgressBar
+            now={percentOfWorkedHours}
+            label={`${percentOfWorkedHours}%`}
+          />
         </div>
         <div className='w-100'></div>
       </div>
