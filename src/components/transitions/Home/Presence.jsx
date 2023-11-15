9240bd49dc03d949548724f959d4e16d7609531c
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-15 10:10:38 +0100

    Update Presence.jsx

--- src/components/transitions/Home/Presence.jsx
+++ src/components/transitions/Home/Presence.jsx
@@ -4,13 +4,13 @@ import { useAuth } from "../Login/AuthContext";
 import ProgressBar from "react-bootstrap/ProgressBar";
 const Presence = ({ user, date }) => {
   const { setMessage, setShowPopUpLogout } = useAuth();
-  const [workedHours, setWorkedHours] = useState(0);
+  const [percentOfWorkedHours, setPercentWorkedHours] = useState(0);
 
   const getInf = async () => {
     try {
       const data = await hoursWorkedApi(user.ID, date);
       console.log(data[0]);
-      setWorkedHours(data[0].SumaGodzin);
+      setPercentWorkedHours(data[0].SumaGodzin);
     } catch (error) {
       console.log(error);
       setMessage(error.message);
