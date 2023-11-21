1d49bfd36c828540547bef5b04e968649aed5751
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-21 13:35:32 +0100

    Update PositionForm.jsx

--- src/components/transitions/Employees/Edit/PositionForm.jsx
+++ src/components/transitions/Employees/Edit/PositionForm.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { supervisorsApi } from "../../../../api/employeesApi";
 import { departmentsApi } from "../../../../api/departmentsApi";
 import { positionApi } from "../../../../api/positionApi";
 import { workingTimeApi } from "../../../../api/workingTimeApi";
-import { subordinatesApi } from "../../../../api/employeesApi";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import { useAuth } from "../../Login/AuthContext";
@@ -21,137 +20,106 @@ const PositionForm = (props) => {
     workingTime,
     setWorkingTime,
   } = props;
-
   const { setShowPopUpLogout, setMessage } = useAuth();
   const [departments, setDepartments] = useState([]);
   const [positions, setPositions] = useState([]);
   const [supervisors, setSupervisors] = useState([]);
   const [workingTimes, setWorkingTimes] = useState([]);
 
-  const onChangeDepartment = (event) => {
-    setDepartment(event.target.value);
-  };
-  const onChangePosition = (event) => {
-    setPosition(event.target.value);
-  };
-  const onChangeSupervisor = (event) => {
-    console.log(event.target.value);
-    setSupervisor(event.target.value);
-  };
-  const onChangeWorkingTime = (event) => {
-    setWorkingTime(event.target.value);
-  };
+  const onChange = useCallback((event, setter) => {
+    setter(event.target.value);
+  }, []);
 
-  const getFromApi = async (setStateTab, api, value) => {
+  const fetchData = async (api, args) => {
     try {
-      value === undefined
-        ? setStateTab(await api())
-        : setStateTab(await api(value));
+      return await api(args);
     } catch (error) {
       console.error(error);
       setMessage(error.message);
       setShowPopUpLogout(true);
+      return [];
     }
   };
 
-  const getSupervisors = async () => {
-    try {
-      const data = await supervisorsApi();
-      let newTab = data.filter(
-        (supervisor) => !subordinates.includes(supervisor.ID)
+  const filterSupervisors = (data) => {
+    let filteredSupervisors = data.filter(
+      (supervisor) => !subordinates.includes(supervisor.ID)
+    );
+    if (employee.ID !== 1) {
+      filteredSupervisors = filteredSupervisors.filter(
+        (supervisor) => supervisor.ID !== employee.ID
       );
-      if (employee.ID !== 1) {
-        newTab = newTab.filter((supervisor) => supervisor.ID !== employee.ID);
-      }
-      if (employee.PrzelozonyID === null) {
-        const supervisorsInSpecificDepartment = newTab.filter(
-          (supervisor) => supervisor.Dzial_ID === department
-        );
-        if (supervisorsInSpecificDepartment.length)
-          setSupervisor(supervisorsInSpecificDepartment[0].ID);
-      }
-      setSupervisors(newTab);
-    } catch (error) {
-      console.error(error);
-      setMessage(error.message);
-      setShowPopUpLogout(true);
     }
+    return filteredSupervisors;
   };
 
   useEffect(() => {
-    getFromApi(setDepartments, departmentsApi);
-    getFromApi(setWorkingTimes, workingTimeApi);
-    if (employee) {
-      setDepartment(employee.DzialID);
-      setPosition(employee.StanowiskoID);
-      setWorkingTime(employee.WymiarPracy_ID);
-      setSupervisor(employee.PrzelozonyID);
-    } else {
-      setDepartment(departments[2].ID);
-    }
+    fetchData(departmentsApi).then(setDepartments);
+    fetchData(workingTimeApi).then(setWorkingTimes);
+  }, []);
+
+  useEffect(() => {
+    fetchData(positionApi, department || 1).then(setPositions);
+  }, [department]);
+
+  useEffect(() => {
+    fetchData(supervisorsApi).then((data) =>
+      setSupervisors(filterSupervisors(data))
+    );
   }, [subordinates]);
 
   useEffect(() => {
-    getSupervisors();
-    department !== undefined
-      ? getFromApi(setPositions, positionApi, department)
-      : getFromApi(setPositions, positionApi, 1);
-  }, [department]);
+    setDepartment(employee?.DzialID || departments[2]?.ID);
+    setPosition(employee?.StanowiskoID || positions[0]?.ID);
+    setWorkingTime(employee?.WymiarPracy_ID || workingTimes[0]?.ID);
+    setSupervisor(employee?.PrzelozonyID || supervisors[0]?.ID);
+  }, [subordinates]);
+
+  const renderSelect = (label, value, onChange, options, formatOption) => (
+    <FloatingLabel controlId={`floatingSelect-${label}`} label={label}>
+      <Form.Select
+        value={value}
+        aria-label={`Floating label select example ${label}`}
+        onChange={onChange}>
+        {options.map((option, index) => (
+          <option value={option.ID} key={option.ID}>
+            {formatOption(option)}
+          </option>
+        ))}
+      </Form.Select>
+    </FloatingLabel>
+  );
 
   return (
     <>
-      <FloatingLabel controlId='floatingSelect' label='Dział'>
-        <Form.Select
-          value={department}
-          aria-label='Floating label select example'
-          onChange={onChangeDepartment}>
-          {departments.map((department) => (
-            <option value={department.ID} key={department.ID}>
-              {department.Nazwa}
-            </option>
-          ))}
-        </Form.Select>
-      </FloatingLabel>
-      <FloatingLabel controlId='floatingSelect' label='Stanowisko'>
-        <Form.Select
-          value={position}
-          aria-label='Floating label select example'
-          onChange={onChangePosition}>
-          {positions.map((position) => (
-            <option value={position.ID} key={position.ID}>
-              {position.Nazwa}
-            </option>
-          ))}
-        </Form.Select>
-      </FloatingLabel>
-      <FloatingLabel controlId='floatingSelect' label='Przełozony'>
-        <Form.Select
-          value={supervisor || employee.ID}
-          aria-label='Floating label select example'
-          onChange={onChangeSupervisor}>
-          {supervisors.map((supervisor) => (
-            <option value={supervisor.ID} key={supervisor.ID}>
-              {supervisor.Imie +
-                " " +
-                supervisor.Nazwisko +
-                " - " +
-                supervisor.Nazwa}
-            </option>
-          ))}
-        </Form.Select>
-      </FloatingLabel>
-      <FloatingLabel controlId='floatingSelect' label='Czas pracy'>
-        <Form.Select
-          value={workingTime}
-          aria-label='Floating label select example'
-          onChange={onChangeWorkingTime}>
-          {workingTimes.map((workingTime) => (
-            <option value={workingTime.ID} key={workingTime.ID}>
-              {workingTime.Od + " - " + workingTime.Do}
-            </option>
-          ))}
-        </Form.Select>
-      </FloatingLabel>
+      {renderSelect(
+        "Dział",
+        department,
+        (e) => onChange(e, setDepartment),
+        departments,
+        (d) => d.Nazwa
+      )}
+      {renderSelect(
+        "Stanowisko",
+        position,
+        (e) => onChange(e, setPosition),
+        positions,
+        (p) => p.Nazwa
+      )}
+      {renderSelect(
+        "Przełożony",
+        supervisor || employee.ID,
+        (e) => onChange(e, setSupervisor),
+        supervisors,
+        (s) => `${s.Imie} ${s.Nazwisko} - ${s.Nazwa}`
+      )}
+      {renderSelect(
+        "Czas pracy",
+        workingTime,
+        (e) => onChange(e, setWorkingTime),
+        workingTimes,
+        (w) => `${w.Od} - ${w.Do}`
+      )}
     </>
   );
 };
