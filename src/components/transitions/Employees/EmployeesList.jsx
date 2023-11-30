c81072a7c4aeff7c3bfc3c1fd3aecfb8a6dd923d
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-30 16:42:44 +0100

    Update EmployeesList.jsx

--- src/components/transitions/Employees/EmployeesList.jsx
+++ src/components/transitions/Employees/EmployeesList.jsx
@@ -2,39 +2,33 @@ import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import EmployeeListItem from "./EmployeeListItem";
 import RenderChildren from "../../common/RenderChildren";
-
 const EmployeesList = React.memo((props) => {
   const { subordinates, date, children, showWorkedHours, dzial } = props;
 
   const renderEmployees = (employees, dept) => {
-    const employeeItems = employees.map((employee, index) => (
-      <EmployeeListItem
-        employee={employee}
-        date={date}
-        key={`${dept}-${index}`}
-        showWorkedHours={showWorkedHours}>
-        {React.Children.map(children, (child) => {
-          if (React.isValidElement(child)) {
-            return React.cloneElement(child, { employee: employee });
-          }
-          return child;
-        })}
-      </EmployeeListItem>
-    ));
-
-    // Filtruj puste elementy
-    const nonEmptyEmployeeItems = employeeItems.filter((item) => !!item);
-
-    return nonEmptyEmployeeItems;
+    return employees
+      .map((employee, index) => (
+        <EmployeeListItem
+          employee={employee}
+          date={date}
+          key={`${dept}-${index}`}
+          showWorkedHours={showWorkedHours}>
+          {React.Children.map(children, (child) => {
+            if (React.isValidElement(child)) {
+              return React.cloneElement(child, { employee: employee });
+            }
+            return child;
+          })}
+        </EmployeeListItem>
+      ))
+      .filter((item) => !!item); // Filtruj puste elementy
   };
 
   const renderDepartment = (department, employees) => {
     const departmentContent = renderEmployees(employees, department);
 
-    const hasNonEmptyContent = departmentContent.some((item) => !!item);
-
-    if (!hasNonEmptyContent) {
-      return null; // Jeśli nie ma niepustych elementów, nie renderuj department
+    if (departmentContent.length === 0) {
+      return null; // Jeśli w dziale nie ma pracowników, nie renderuj działu
     }
     return (
       <Accordion.Item eventKey={department} key={department}>
@@ -50,16 +44,14 @@ const EmployeesList = React.memo((props) => {
     <Accordion className='scroll'>
       {dzial === "Każdy" ? (
         typeof subordinates === "object" ? (
-          Object.entries(subordinates).map(([dept, employees]) =>
-            renderDepartment(dept, employees)
-          )
+          Object.entries(subordinates)
+            .map(([dept, employees]) => renderDepartment(dept, employees))
+            .filter((department) => department != null) // Filtruj puste działy
         ) : (
           <div className='no-data-message'>Brak danych</div>
         )
-      ) : Array.isArray(subordinates) && subordinates.length > 0 ? (
-        renderEmployees(subordinates, dzial)
       ) : (
-        <div className='no-data-message'>Brak danych</div>
+        renderDepartment(dzial, subordinates)
       )}
     </Accordion>
   );
