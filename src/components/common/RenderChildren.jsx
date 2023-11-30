90d03baddb516ed8e118ad54a24fe9771cf17f9c
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-30 14:12:32 +0100

    First

--- src/components/common/RenderChildren.jsx
+++ src/components/common/RenderChildren.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const RenderChildren = (props) => {
   console.log("sprawdzam czy ma dzieci");
-  const hasNonNullChildren = React.Children.toArray(props.children).some(
-    (child) => {
-      return child !== null && child !== undefined;
-    }
-  );
-  hasNonNullChildren ? console.log("Ma dzieci") : console.log("Nie ma dzieci");
+  const hasContent = React.Children.toArray(props.children).some((child) => {
+    // Jeśli dziecko jest tekstem, JSX lub generuje treść, uważamy, że zawiera treść
+    return (
+      typeof child === "string" ||
+      React.isValidElement(child) ||
+      (child && child.props && child.props.children)
+    );
+  });
+  hasContent ? console.log("Ma dzieci") : console.log("Nie ma dzieci");
 
-  if (hasNonNullChildren) {
+  if (hasContent) {
     return <>{props.children}</>;
   } else {
     return <h3>Brak zawartości.</h3>;
--- src/components/transitions/Employees/EmployeesList.jsx
+++ src/components/transitions/Employees/EmployeesList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import EmployeeListItem from "./EmployeeListItem";
 import RenderChildren from "../../common/RenderChildren";
